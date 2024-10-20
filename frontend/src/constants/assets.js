0e9c87e0c58181992850ef07cf785626026f0eac
eunivateDev <eunivate@gmail.com>
2024-10-20 21:21:47 +0800

    Add files via upload

---
+++ frontend/src/constants/assets.js
@@ -0,0 +1,315 @@
+
+import cisco from '../assets/cisco.png';
+import close from '../assets/close.png';
+import downArrow from '../assets/downArrow.png';
+import fifty from '../assets/fifty.png';
+import light from '../assets/light.png';
+import menu from '../assets/menu.png';
+import oracle from '../assets/oracle.png';
+import proj_man from '../assets/proj_man.png'; 
+import sap from '../assets/sap.png';
+import star_sea from '../assets/star_sea.png';
+import twenty from '../assets/twenty.png';
+import webinar from '../assets/webinar.png';
+import thinkTogether from '../assets/thinkTogether.png';
+import indMeets from '../assets/indMeets.png';
+import futureSolution from '../assets/futureSolution.png';
+import yellow_gradient from '../assets/yellow_gradient.svg';
+import red_gradient from '../assets/red_gradient.svg';
+import loop from '../assets/loop.svg';
+import OksYun from '../assets/OksYun.png';
+// LogoSlides Assets - Line 1
+import logo1_ln1 from '../assets/LogoSlides/logo1_ln1.png';
+import logo2_ln1 from '../assets/LogoSlides/logo2_ln1.png';
+import logo3_ln1 from '../assets/LogoSlides/logo3_ln1.png';
+import logo4_ln1 from '../assets/LogoSlides/logo4_ln1.png';
+import logo5_ln1 from '../assets/LogoSlides/logo5_ln1.png';
+import logo18_ln1 from '../assets/LogoSlides/logo18_ln1.png';
+import logo19_ln1 from '../assets/LogoSlides/logo19_ln1.png';
+import logo20_ln1 from '../assets/LogoSlides/logo20_ln1.png';
+import logo21_ln1 from '../assets/LogoSlides/logo21_ln1.png';
+import logo22_ln1 from '../assets/LogoSlides/logo22_ln1.png';
+import logo31_ln1 from '../assets/LogoSlides/logo31_ln1.png';
+import logo32_ln1 from '../assets/LogoSlides/logo32_ln1.png';
+import logo35_ln1 from '../assets/LogoSlides/logo35_ln1.png';
+// LogoSlides Assets - Line 2
+import logo6_ln2 from '../assets/LogoSlides/logo6_ln2.png';
+import logo7_ln2 from '../assets/LogoSlides/logo7_ln2.png';
+import logo8_ln2 from '../assets/LogoSlides/logo8_ln2.png';
+import logo9_ln2 from '../assets/LogoSlides/logo9_ln2.png';
+import logo10_ln2 from '../assets/LogoSlides/logo10_ln2.png';
+import logo11_ln2 from '../assets/LogoSlides/logo11_ln2.png';
+import logo27_ln2 from '../assets/LogoSlides/logo27_ln2.png';
+import logo26_ln2 from '../assets/LogoSlides/logo26_ln2.png';
+import logo25_ln2 from '../assets/LogoSlides/logo25_ln2.png';
+import logo23_ln2 from '../assets/LogoSlides/logo23_ln2.png';
+import logo24_ln2 from '../assets/LogoSlides/logo24_ln2.png';
+import logo2_ln2 from '../assets/LogoSlides/logo2_ln2.png';
+// LogoSlides Assets - Line 3
+import logo34_ln3 from '../assets/LogoSlides/logo34_ln3.png';
+import logo32_ln3 from '../assets/LogoSlides/logo32_ln3.png';
+import logo10_ln3 from '../assets/LogoSlides/logo10_ln3.png';
+import logo12_ln3 from '../assets/LogoSlides/logo12_ln3.png';
+import logo33_ln3 from '../assets/LogoSlides/logo33_ln3.png';
+import logo11_ln3 from '../assets/LogoSlides/logo11_ln3.png';
+import logo26_ln3 from '../assets/LogoSlides/logo26_ln3.png';
+import logo13_ln3 from '../assets/LogoSlides/logo13_ln3.png';
+import logo31_ln3 from '../assets/LogoSlides/logo31_ln3.png';
+import logo9_ln3 from '../assets/LogoSlides/logo9_ln3.png';
+import logo14_ln3 from '../assets/LogoSlides/logo14_ln3.png';
+import logo28_ln3 from '../assets/LogoSlides/logo28_ln3.png';
+import logo29_ln3 from '../assets/LogoSlides/logo29_ln3.png';
+// LogoSlides Assets - Line 4
+import logo25_ln4 from '../assets/LogoSlides/logo25_ln4.png';
+import logo40_ln4 from '../assets/LogoSlides/logo40_ln4.png';
+import logo14_ln4 from '../assets/LogoSlides/logo14_ln4.png';
+import logo15_ln4 from '../assets/LogoSlides/logo15_ln4.png';
+import logo7_ln4 from '../assets/LogoSlides/logo7_ln4.png';
+import logo16_ln4 from '../assets/LogoSlides/logo16_ln4.png';
+import logo17_ln4 from '../assets/LogoSlides/logo17_ln4.png';
+import logo8_ln4 from '../assets/LogoSlides/logo8_ln4.png';
+import logo1_ln4 from '../assets/LogoSlides/logo1_ln4.png';
+import logo10_ln4 from '../assets/LogoSlides/logo10_ln4.png';
+import logo33_ln4 from '../assets/LogoSlides/logo33_ln4.png';
+import logo28_ln4 from '../assets/LogoSlides/logo28_ln4.png';
+
+/* Services */
+import Services_IMG from'../assets/Services_IMG.png';
+/* Products */
+import p1 from '../assets/p1.jpg';
+import p2 from '../assets/p2.jpg';
+import p3 from '../assets/p3.jpg';
+import dev2 from '../assets/dev2.jpg';
+import dev1 from '../assets/dev1.jpg'; 
+import dev3 from '../assets/dev3.jpg';
+import link1 from '../assets/link1.jpg';
+import link2 from '../assets/link2.jpg';
+import link3 from '../assets/link3.jpg';
+import arrowIcon from '../assets/arrowIcon.jpg'; 
+import twitterIcon from '../assets/twitterIcon.jpg'; 
+import linkedinIcon from '../assets/linkedinIcon.jpg';
+import prof1 from '../assets/prof1.jpg';
+import prof2 from '../assets/prof2.jpg';
+import prof3 from '../assets/prof3.jpg';
+import prof4 from '../assets/prof4.jpg';
+
+//Quotation Step4
+import Step4 from '../assets/Step4-quotation.svg';
+import waitingverification from '../assets/verifying.gif';
+import emailsent from '../assets/emailsent.png';
+import submitquotation from '../assets/submitquotation.png';
+
+
+//Login Back
+import Loginback from '../assets/loginback.png';
+
+/* FOOTER IMG */
+import webinar1 from '../assets/webinar1.png';
+import webinar2 from '../assets/webinar2.png';
+import webinar3 from '../assets/webinar3.png';
+
+/* ABOUT US HERO IMG */
+import image1 from '../assets/abt1.png';
+import image2 from '../assets/abt2.png';
+import background from '../assets/backg.png';
+
+/* ABOUT US MISSION IMG */
+import road from '../assets/road.png';
+
+/* ABOUT US ADVANTAGE IMG */
+import Pie1 from '../assets/pie1.png';
+import Pie2 from '../assets/pie2.png';
+import Pie3 from '../assets/pie3.png';
+import Pie4 from '../assets/pie4.png';
+
+
+
+// Admin Img
+import dashboard_logo from '../assets/dashboard_logo.png';
+import dashboard_sidenav_icon from '../assets/dashboard_sidenav_icon.svg';
+import dashboard_sidenav_icon_hover from '../assets/dashboard_sidenav_icon_hover.svg';
+
+/* ICON ADMIN */
+import activity_icon from '../assets/icons/activity-icon.png';
+import messages_icon from '../assets/icons/messages-icon.png';
+import people_icon from '../assets/icons/people-icon.png';
+import project_icon from '../assets/icons/project-icon.png';
+import settings_icon from '../assets/icons/settings-icon.png';
+import task_icon from '../assets/icons/task-icon.png';
+
+/* RED ICON ADMIN */
+import activity_red from '../assets/Redicon/activity-red.png';
+import dashboard_red from '../assets/Redicon/dashboard-red.png';
+import messages_red from '../assets/Redicon/messages-red.png';
+import people_red from '../assets/Redicon/people-red.png';
+import project_red from '../assets/Redicon/project-red.png';
+import settings_red from '../assets/Redicon/settings-red.png';
+import task_red from '../assets/Redicon/task-red.png';
+
+/* ADMIN SETTINGS IMG */
+import User from '../assets/User.png';
+// Admin Dashboard
+import i1 from '../assets/i1.jpg';  // Import your images
+import i2 from '../assets/i2.jpg';
+import i3 from '../assets/i3.jpg';
+import i4 from '../assets/i4.jpg';
+
+
+export {
+  /* RED ICON ADMIN */
+  activity_red,
+  dashboard_red,
+  messages_red,
+  people_red,
+  project_red,
+  settings_red,
+  task_red,
+
+  /* ICON ADMIN */
+  activity_icon,
+  messages_icon,
+  people_icon,
+  project_icon,
+  settings_icon,
+  task_icon,
+
+  /* ADMIN SETTINGS IMG */
+  User,
+  
+    yellow_gradient,
+    red_gradient,
+    loop,
+    cisco,
+    close,
+    downArrow,
+    fifty,
+    light, 
+    menu, 
+    oracle,
+    proj_man,
+    sap,
+    star_sea,
+    twenty,
+    webinar,
+    thinkTogether,
+    indMeets,
+    futureSolution,
+    OksYun,
+
+      /* Services */
+      Services_IMG,
+
+     /* FOOTER IMG */
+    webinar1,
+    webinar2,
+    webinar3,
+
+    /* ABOUT US HERO IMG */
+    image1,
+    image2,  
+    background,
+
+    /* ABOUT US MISSION IMG */
+    road,
+
+    /* ABOUT US ADVANTAGE IMG */
+    Pie1,
+    Pie2,
+    Pie3,
+    Pie4,
+    //Admin Img
+    dashboard_logo, 
+    dashboard_sidenav_icon,
+    dashboard_sidenav_icon_hover,
+    
+    //Admin Dashboard
+    i1,
+    i2,
+    i3,
+    i4,
+
+
+    // LogoSlides Assets - Line 1
+    logo1_ln1,
+    logo2_ln1,
+    logo3_ln1,
+    logo4_ln1,
+    logo5_ln1,
+    logo18_ln1,
+    logo19_ln1,
+    logo20_ln1,
+    logo21_ln1,
+    logo22_ln1,
+    logo31_ln1,
+    logo32_ln1,
+    logo35_ln1,
+    // LogoSlides Assets - Line 2
+    logo6_ln2,
+    logo7_ln2,
+    logo8_ln2,
+    logo9_ln2,
+    logo10_ln2,
+    logo11_ln2,
+    logo27_ln2,
+    logo26_ln2,
+    logo25_ln2,
+    logo23_ln2,
+    logo24_ln2,
+    logo2_ln2,
+     // LogoSlides Assets - Line 3
+     logo34_ln3,
+     logo32_ln3,
+     logo10_ln3,
+     logo12_ln3,
+     logo33_ln3,
+     logo11_ln3,
+     logo26_ln3,
+     logo13_ln3,
+     logo31_ln3,
+     logo9_ln3,
+     logo14_ln3,
+     logo28_ln3,
+     logo29_ln3,
+      // LogoSlides Assets - Line 4
+      logo25_ln4,
+      logo40_ln4,
+      logo14_ln4,
+      logo15_ln4,
+      logo7_ln4,
+      logo16_ln4,
+      logo17_ln4,
+      logo8_ln4,
+      logo1_ln4,
+      logo10_ln4,
+      logo33_ln4,
+      logo28_ln4,
+       
+      // Products Assets
+      p1,
+      p2,
+      p3,
+      dev2,
+      dev1,
+      dev3,
+      link1,
+      link2,
+      link3,
+      arrowIcon,
+      twitterIcon,
+      linkedinIcon,
+      prof1,
+      prof2,
+      prof3,
+      prof4,
+      
+      //Quotation Step4
+      Step4,
+      waitingverification,
+      emailsent,
+      submitquotation,
+
+            //Quotation Step4
+            Loginback
+
+            
+};
---
+++ frontend/src/constants/constants.js
@@ -0,0 +1,51 @@
+// import webinar from '../assets/webinar.png';
+
+// useEffect(() => {
+//   const user = JSON.parse(localStorage.getItem('user'));
+//   if (user) {
+//     setUserName(`${user.firstName} ${user.lastName}`);
+//     setIsAuthenticated(true);
+//   }
+// }, []);
+
+// const navLinks = [
+//   {
+//     id: "Home", path: "/",
+//     title: "Home",
+//     hasDropdown: false,
+//   },
+//   {
+//     id: "Services",
+//     title: "Services",
+//     hasDropdown: true,
+//     subLinks: [
+//       { id: "showcases", title: "Showcases", path: "/showcases" },
+//       {
+//         id: "quotation",
+//         title: "Quotation",
+//         path: isAuthenticated ? "/quotation" : "/login", // Change path based on authentication
+//       },
+//       { id: "eustore", title: "EU Store", path: "/eu-store" },
+//     ],
+//     imageUrl: webinar,
+//     description: 'IT Solution',
+//   },
+//   {
+//     id: "resources",
+//     title: "Resources",
+//     hasDropdown: true,
+//     subLinks: [
+//       { id: "webinars", title: "Webinars", path: "/webinar" },
+//       { id: "events", title: "Events", path: "/events" },
+//       { id: "challenges", title: "Challenges", path: "/challenges" },
+//       { id: "projectManagement", title: "Project Management", path: "/login" },
+//     ],
+//     imageUrl: webinar,
+//     description: 'WEBINAR',
+//   },s
+//   {
+//     id: "aboutus", path: "/about",
+//     title: "About Us",
+//   },
+// ];
+
